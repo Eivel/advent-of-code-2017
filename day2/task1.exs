669f75948bc52a13d67672611d8cd5f1868bfe3b
Wojciech Zagórski <wojaqu@gmail.com>
2017-12-02 19:40:24 +0100

    finish task1/day2

---
+++ day2/task1.exs
@@ -0,0 +1,8 @@
+File.stream!("task1_input")
+|> Stream.map(&String.trim/1)
+|> Stream.map(&String.split(&1, "	"))
+|> Stream.map(&Enum.map(&1, fn (ch) -> Integer.parse(ch) end))
+|> Stream.map(&Enum.map(&1, fn ({int, _}) -> int end))
+|> Stream.map(&(Enum.max(&1) - Enum.min(&1)))
+|> Enum.sum
+|> IO.puts
