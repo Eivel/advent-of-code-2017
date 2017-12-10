734a2ed303dab010654be6963845116a682d0b3f
Wojciech Zagórski <wojaqu@gmail.com>
2017-12-10 21:28:56 +0100

    compare performance with solution in Go

---
+++ day5/task2.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	arr := readLine("task_input")
+	fmt.Println(step(arr, 0, 0))
+	elapsed := time.Since(start)
+	log.Printf("Calculation took %s", elapsed)
+
+}
+func readLine(path string) []int {
+	arr := make([]int, 0)
+	inFile, _ := os.Open(path)
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		i, _ := strconv.Atoi(scanner.Text())
+		arr = append(arr, i)
+	}
+	return arr
+}
+
+func step(arr []int, position int, steps int) int {
+	for position < len(arr) && position >= 0 {
+		val := arr[position]
+		arr[position] = processOffset(val)
+		position = position + val
+		steps = steps + 1
+	}
+	return steps
+}
+
+func processOffset(offset int) int {
+	if (offset) >= 3 {
+		return offset - 1
+	}
+	return offset + 1
+}
